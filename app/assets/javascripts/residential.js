ffd73f794b6ad7a936f678d665118b87b1a2197c
Dāgs Grīnbergs <dags.grinbergs@cemety.lv>
2017-01-28 14:54:13 +0200

    Refactor pie chart JS

--- app/assets/javascripts/residential.js
+++ app/assets/javascripts/residential.js
@@ -1,4 +1,4 @@
-$(function() {
+function makePie() {
   // From: http://bl.ocks.org/mbostock/3887235
   // Set the dimensions
   var width  = 960,
@@ -33,14 +33,14 @@ $(function() {
         .data(pie(d3.keys(totals)))
       .enter().append("g")
         .attr("class", "arc")
-        	.on("mouseover", function(d) {
-        		d3.select(this).select("text").style("font-weight", "bold")
-        		d3.select(this).select("text").style("font-size", "1.25em")
-        	})
-        	.on("mouseout", function(d) {
-        		d3.select(this).select("text").style("font-weight", "normal")
-        		d3.select(this).select("text").style("font-size", "1em")
-        	});
+    	.on("mouseover", function(d) {
+    		d3.select(this).select("text").style("font-weight", "bold")
+    		d3.select(this).select("text").style("font-size", "1.25em")
+    	})
+    	.on("mouseout", function(d) {
+    		d3.select(this).select("text").style("font-weight", "normal")
+    		d3.select(this).select("text").style("font-size", "1em")
+    	});
 
     // color the pie slices using the color pallet
     g.append("path")
@@ -50,11 +50,9 @@ $(function() {
     // add the jurisdiction name, put the labels outside the pie (in a new arc/circle)
     var pos = d3.svg.arc().innerRadius(radius + 20).outerRadius(radius + 20);
     g.append("text")
-      .attr("transform", function(d) {
-        return "translate(" + pos.centroid(d) + ")";
-       })
+      .attr("transform", function(d) { return "translate(" + pos.centroid(d) + ")"; })
       .attr("dy", ".35em")
       .style("text-anchor", "middle")
       .text(function(d) { return d.data; });
   });
-});
+}
