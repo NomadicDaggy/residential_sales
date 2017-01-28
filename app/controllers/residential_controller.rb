47279065f6b9e1455cef4f10daf2458f1499eb47
Dāgs Grīnbergs <dags.grinbergs@cemety.lv>
2017-01-28 15:23:57 +0200

    Bar chart view actions

--- app/controllers/residential_controller.rb
+++ app/controllers/residential_controller.rb
@@ -2,6 +2,9 @@ class ResidentialController < ApplicationController
   def index
   end
 
+  def bar_chart
+  end
+
   def data
     totals = SalesFigure.group(:jurisdiction).sum(:total_sales)
     render :json => { :totals => totals }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'residential/index'
+  get 'residential/bar_chart'
   get 'residential/data', :defaults => { :format => 'json' }
+  get 'residential/bar_data', :defaults => { :format => 'json' }
   root :to => 'residential#index'
 end
